c1a4dfef9d70d9b57bde4149bd793412b582a193
John Olamide Alokan <alokanolamide@gmail.com>
2025-07-25 13:34:09 +0100

    Fix AWS Amplify build error by downgrading dependencies for Node.js 18 compatibility

---
+++ .eslintrc.cjs
@@ -0,0 +1,21 @@
+// Using CommonJS format for ESLint 8 compatibility
+module.exports = {
+  root: true,
+  env: { browser: true, es2020: true },
+  extends: [
+    'eslint:recommended',
+    'plugin:react-hooks/recommended',
+    'plugin:react-refresh/recommended',
+  ],
+  ignorePatterns: ['dist', '.eslintrc.cjs'],
+  parserOptions: { ecmaVersion: 'latest', sourceType: 'module' },
+  settings: { react: { version: '18.2' } },
+  plugins: ['react-refresh'],
+  rules: {
+    'react-refresh/only-export-components': [
+      'warn',
+      { allowConstantExport: true },
+    ],
+    'no-unused-vars': ['error', { varsIgnorePattern: '^[A-Z_]' }],
+  },
+}
